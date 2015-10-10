07c1665abcd23c7d5d971dd6aabbe4d46285a178
CharliePearce <miss.neophyte@gmail.com>
2015-10-10 13:09:41 +0100

    user input converted to array working

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
+var player1Word;  
+var player1Array;
+
 function wordPrompt(){
-  prompt("What is your chosen word, Player One?");
-}
+  player1Word = prompt("What is your chosen word, Player One?");
+  console.log(player1Word);
+  player1Array = player1Word.split("");
+  console.log(player1Array);
+  }
 
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
