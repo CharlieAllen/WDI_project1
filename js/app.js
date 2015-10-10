72a0e86696fba3298f8aefe5171bf13fd86d2aa9
CharliePearce <miss.neophyte@gmail.com>
2015-10-10 09:50:42 +0100

    player1 prompt box added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
 $(function() {
-    console.log( "ready!" );
+    //console.log( "ready!" );
+
+var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+var wordInput = prompt("Player One, please enter your word");
+  //console.log("word prompt works");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
