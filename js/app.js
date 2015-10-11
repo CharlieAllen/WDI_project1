1edd65971a588132f3cc04e90c28b18232109df4
CharliePearce <miss.neophyte@gmail.com>
2015-10-11 08:54:03 +0100

    guess input box working

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
-var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var player1Word;
 var player1Array;
 var guesses = [];
+var player2Guess = [];
 
 function wordPrompt(){
   player1Word = prompt("What is your chosen word, Player One?");
-  console.log(player1Word);
+  //console.log(player1Word);
   player1Array = player1Word.split("");
-  console.log(player1Array);
-  console.log(player1Array.length);
+  //console.log(player1Array);
+  //console.log(player1Array.length);
   createBoard();
   updateBoard();
 }
@@ -17,9 +17,14 @@ function createBoard(){
   for (var i = 0; i < player1Array.length; i++){
     guesses.push("_");
   }
-  console.log(guesses);
+  //console.log(guesses);
 }
 
 function updateBoard(){
   document.getElementById("gameLetters").innerText = guesses;
+}
+
+function guessMade(){
+  player2Guess = document.getElementById("guessText").value;
+  console.log("Guess: " + player2Guess);
 }
