c510d22a840eaa7c3e471b277f0bf21990db76e4
CharliePearce <miss.neophyte@gmail.com>
2015-10-11 17:49:41 +0100

    counter added

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@ var player1Word;
 var player1Array;
 var guesses = [];
 var player2Guess = [];
+var guessesRemaining;
+var livesLeft = 0;
 
 function wordPrompt(){
   player1Word = prompt("What is your chosen word, Player One?");
@@ -11,6 +13,7 @@ function wordPrompt(){
   //console.log(player1Array.length);
   createBoard();
   updateBoard();
+  updateLives();
 }
 
 function createBoard(){
@@ -18,6 +21,10 @@ function createBoard(){
     guesses.push("_");
   }
   //console.log(guesses);
+}
+
+function updateLives(){
+  document.getElementById("counter").innerText = "You have " + livesLeft + " goes left";
 }
 
 function updateBoard(){
@@ -28,3 +35,36 @@ function guessMade(){
   player2Guess = document.getElementById("guessText").value;
   console.log("Guess: " + player2Guess);
 }
+//the letter doesn't disappear from the input box which is definitely not ideal
+//it also ought to stop you from being able to input the same letter twice
+
+
+function checkForMatch(){
+  //checks the letter input by player2 against those stored, if it matches any
+  //the letter is added to the array displayed
+  for (var i = 0; i < player2Guess; i +=1){
+    if (player2Guess === gameLetters[i])
+      guesses.push([i]);
+  }
+}
+
+function noMatch(){
+  //if player2's guess doesn't match any of the stored letters, 
+  //reveal a square on the grid, increment score -1, stop at 0
+}
+
+function checkForWin(){
+  //check to see if player2 has the same number of matches to letters stored
+  //in player1's word - if yes, they have won & score is incremented
+}
+
+function checkForLose(){
+  //check to see if all squares are uncovered, if so player1 wins, their score
+  //is incremented
+}
+
+//for score it might actually be better to write a for loop which takes the number
+//of letters in the array, -1 from it and shows that as the lives remaining?
+
+
+
